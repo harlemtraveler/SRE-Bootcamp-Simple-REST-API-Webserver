63fd7281161e9632c17d7d37b7146d6dfe4e1e88
Tim <tjk315@gmail.com>
2025-07-12 23:27:40 +0700

    Add 'Student' Model

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from .db import db
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    age = db.Column(db.Integer)
+    grade = db.Column(db.String(10))
